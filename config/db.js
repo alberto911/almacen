c745634568a35a2266662a577ade6caca5dfed44
Alberto Pickering <alberto911_@hotmail.com>
2018-05-08 19:38:40 -0500

    GrapheneDB

--- config/db.js
+++ config/db.js
@@ -1,3 +1,13 @@
-var db = require('seraph')({ pass: 'Ariel' });
+if (process.env.GRAPHENEDB_URL) {
+  var url = require('url').parse(process.env.GRAPHENEDB_URL)
+
+  var db = require("seraph")({
+    server: url.protocol + '//' + url.host,
+    user: url.auth.split(':')[0],
+    pass: url.auth.split(':')[1]
+  });
+}
+else
+  var db = require('seraph')({ pass: 'Ariel' });
 
 module.exports = db;
